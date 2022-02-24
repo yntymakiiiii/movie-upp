a8b57fcef9e9c24ee6fdaa408a0c5fadc566c347
yntymakiiiii <yntymakorozbekov3@gmail.com>
2022-02-24 17:54:09 +0600

    Кыйналбачы

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(".movie").slick({
+    slidesToShow: 4,
+    dots:true,
+    centerMode:true,
+    centerPadding: "15px"
+})
+
+
+$(".movie1").slick({
+    slidesToShow: 4,
+    dots: true
+})
+$(".movie2").slick({
+    slidesToShow: 3,
+    dots:true,
+    centerMode:true,
+    centerPadding: "15px"
+})
+$(".video").slick({
+    slidesToShow: 2,
+    dots:true,
+    centerMode:true,
+    centerPadding: "15px"
+})
